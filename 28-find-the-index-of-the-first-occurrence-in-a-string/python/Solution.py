db6b3f9dc65c78130d9fc5d743f15d08636fbbd9
maximkovalev <mkovalevhse@yandex.ru>
2023-09-20 11:17:58 +0300

    chore(python): Added new python solution (Take 2)

--- 28-find-the-index-of-the-first-occurrence-in-a-string/python/Solution.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/python/Solution.py
@@ -1,12 +1,19 @@
-def get_overlap_index(haystack: str, needle: str) -> int:
-    haystack_length: int = haystack.__len__()
-    needle_length: int = needle.__len__()
+class Solution(object):
+    @staticmethod
+    def strStr(haystack: str, needle: str) -> int:
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        haystack_length = len(haystack)
+        needle_length = len(needle)
 
-    if haystack_length < needle_length:
+        if haystack_length < needle_length:
+            return -1
+
+        for i in range(haystack_length - needle_length):
+            if haystack[i:i + needle_length] == needle:
+                return i
+
         return -1
-
-    for i in range(haystack_length - needle_length):
-        if haystack[i:i + needle_length] == needle:
-            return i
-
-    return -1
