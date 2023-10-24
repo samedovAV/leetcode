745036284426d0d076993c1e3a72deab280d5cba
Aleksandr Samedov <alekcandr206@gmail.com>
2023-10-24 22:59:28 +0200

    feat(java, solved)11: containter with most water

---
+++ 11-container-with-most-water/java/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int left = 0;
+        int right = heights.length - 1;
+        int max = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int height = Math.min(heights[left], heights[right]);
+            int area = width * height;
+
+            max = Math.max(max, area);
+
+            if(heights[left] < heights[right]) {
+                left++;
+            } else if(heights[left] > heights[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return max;
+    }
+}
